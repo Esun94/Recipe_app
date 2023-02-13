97a68f265beb2ff57a23c19e17e3174d4b60fb4d
Eric Sun <ericmsun@gmail.com>
2023-02-13 16:21:18 -0500

    created modal view for instructions on Random Recipe component, passed instruction and title props to Modal component

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -30,21 +30,21 @@ function Random() {
             <div className='grid grid-cols-4 p-8'>
                 {random.map((recipe) => {
                     return (
-                        <section key={recipe.id}>
+                        <section key={recipe.id} className="m-6">
                             <div className="border rounded-lg overflow-hidden shadow-lg relative h-96 min-h-full mx-6 " key={recipe.id}>
                                 <div className='border rounded-lg bg-gradient-to-t from-gray-900 to-transparent absolute w-full h-full'>
                                     <img className=" border rounded-lg w-full h-full absolute object-cover left-0 -z-10" src={recipe.image} alt="prepared food"></img>
                                 </div>
                                 <p className="font-bold text-xl m-2 absolute z-10 bottom-0 w-full h-2/5 text-center flex justify-center items-center left-1/2 -translate-x-1/2 translate-y-0 text-white">{recipe.title}</p>
-                                <div dangerouslySetInnerHTML={{ __html: recipe.instructions }}>
+                                {/* <div dangerouslySetInnerHTML={{ __html: recipe.instructions }}> */}
                                     {/* {renderInstruction(recipe.instructions)} */}
-                                </div>
+                                {/* </div> */}
                             </div>
+                            <Modal title={recipe.title} instructions={recipe.instructions}/>
                         </section>
                     );
                 })}
             </div>
-            <Modal />
             <div className='flex justify-center'>
                 <button className='border border-red-600 p-2 bg-red-400 rounded-xl' onClick={getRandom}>Random Recipes!</button>
             </div>
